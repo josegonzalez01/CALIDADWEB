2ad40b9ccf187a74d8223c69793937dbf8a4210a
josealexander <gonzalez_ingsis_96@hotmail.com>
2020-06-06 18:37:30 -0500

    Subida de archivos

---
+++ js/conexion_whatsapp.js
@@ -0,0 +1,4 @@
+/* <![CDATA[ */
+var ht_ctc_var = {"is_mobile":"no","post_title":"Home","is_ga_enable":"no","is_fb_an_enable":"no"};
+var ht_ctc_var_chat = {"number":"51922068084","pre_filled":"Estimado notario, mi consulta es la siguiente: ","show_or_hide":"hide","webandapi":""};
+/* ]]> */
